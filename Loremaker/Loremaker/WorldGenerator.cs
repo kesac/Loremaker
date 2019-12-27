47e2a8ed7b9e323e02c1bd3231c3703b817608c8
kesac <kevin@turtlesort.com>
2019-12-27 02:27:27 -0700

    Started implementation of WorldGenerator class

---
+++ Loremaker/Loremaker/WorldGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Loremaker.Names;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker
+{
+    public class WorldGenerator
+    {
+
+        private ILocationNameGenerator LocationNameGenerator { get; set; }
+
+        public WorldGenerator(ILocationNameGenerator generator)
+        {
+            this.LocationNameGenerator = generator;
+        }
+
+        public World Next()
+        {
+            var world = new World();
+            world.Name = this.LocationNameGenerator.NextWorldName();
+
+            return world;
+        }
+
+    }
+}
