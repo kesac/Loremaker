838aa241008adee482de4e889bb6a1ce6d7b062d
kesac <kevin@turtlesort.com>
2019-12-27 02:28:27 -0700

    Added Loremaker.Example project to contain examples of class library usage

---
+++ Loremaker/Loremaker.Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using Loremaker.Names;
+using System;
+
+namespace Loremaker.Example
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var generator = new WorldGenerator(new DefaultNameGenerator());
+
+            var world = generator.Next();
+        }
+    }
+}
