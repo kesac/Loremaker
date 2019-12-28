e15feffcc74cd2e5debfce3bf37e82d709a05b1a
kesac <kevin@turtlesort.com>
2019-12-27 23:57:34 -0700

    Finished implementation of DefaultHeightMapGenerator. Fixed an issue when getting width and height of map. Added flag SeedCorners for disabling seeding corners of the map with non-zero values.

--- Loremaker/Loremaker/Maps/DefaultHeightMapGenerator.cs
+++ Loremaker/Loremaker/Maps/DefaultHeightMapGenerator.cs
@@ -11,9 +11,12 @@ namespace Loremaker.Maps
     {
         private Random Random { get; set; }
 
+        public bool SeedCorners { get; set; }
+
         public DefaultHeightMapGenerator()
         {
             this.Random = new Random();
+            this.SeedCorners = true;
         }
 
         public double[,] Next(int width, int height)
@@ -22,19 +25,33 @@ namespace Loremaker.Maps
             // In order to generate maps of any dimension, we generate a 2^n+1 map that
             // is larger than the desired dimensions than remove the "extra" parts.
 
-            double exponentForWidth = Math.Log(width) / Math.Log(2);
-            double exponentForHeight = Math.Log(height) / Math.Log(2);
-
-            if(exponentForWidth < exponentForHeight)
+            if(width < height)
             {
-                // TODO: remove the extra data
-                return this.GenerateHeightMap(Convert.ToInt32(Math.Pow(2, Math.Ceiling(exponentForWidth)) + 1));
+                double exponentForWidth = Math.Log(width) / Math.Log(2);
+                var map = this.GenerateHeightMap(Convert.ToInt32(Math.Pow(2, Math.Ceiling(exponentForWidth)) + 1));
+                return ShrinkArray(map, width, height);  
+                
             }
             else
             {
-                return this.GenerateHeightMap(Convert.ToInt32(Math.Pow(2, Math.Ceiling(exponentForHeight)) + 1));
+                double exponentForHeight = Math.Log(height) / Math.Log(2);
+                var map = this.GenerateHeightMap(Convert.ToInt32(Math.Pow(2, Math.Ceiling(exponentForHeight)) + 1));
+                return ShrinkArray(map, width, height);
             }
 
+        }
+
+        private double[,] ShrinkArray(double[,] original, int targetWidth, int targetHeight)
+        {
+            var result = new double[targetWidth, targetHeight];
+            for (int i = 0; i < targetWidth; i++)
+            {
+                for(int j = 0; j < targetHeight; j++)
+                {
+                    result[i, j] = original[i, j];
+                }
+            }
+            return result;
         }
 
         // This implementation is based off 'hmapgen' (https://github.com/kesac/hmapgen).
@@ -42,10 +59,13 @@ namespace Loremaker.Maps
         {
             double[,] map = new double[mapsize,mapsize];
 
-            map[0, 0] = this.Random.NextDouble();
-            map[0, mapsize - 1] = this.Random.NextDouble();
-            map[mapsize - 1, 0] = this.Random.NextDouble();
-            map[mapsize - 1, mapsize - 1] = this.Random.NextDouble();
+            if (this.SeedCorners)
+            {
+                map[0, 0] = this.Random.NextDouble() / 2 + 0.25;
+                map[0, mapsize - 1] = this.Random.NextDouble() / 2 + 0.25;
+                map[mapsize - 1, 0] = this.Random.NextDouble() / 2 + 0.25;
+                map[mapsize - 1, mapsize - 1] = this.Random.NextDouble() / 2 + 0.25;
+            }
 
             int step = mapsize - 1;
             double variance = 1;
@@ -114,7 +134,7 @@ namespace Loremaker.Maps
                 average += map[x - step / 2, y];
             }
 
-            if (x + step / 2 < map.Rank)
+            if (x + step / 2 < map.GetLength(0))
             {
                 count++;
                 average += map[x + step / 2, y];
@@ -126,7 +146,7 @@ namespace Loremaker.Maps
                 average += map[x, y - step / 2];
             }
 
-            if (y + step / 2 < map.Rank)
+            if (y + step / 2 < map.GetLength(1))
             {
                 count++;
                 average += map[x, y + step / 2];
