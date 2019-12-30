8804dada2159e36a8d6d8d7c06c050f5cbf3d5fc
kesac <kevin@turtlesort.com>
2019-12-30 02:16:30 -0700

    Added IslandHeightMapGenerator that can make height maps with large islands or continents

---
+++ Loremaker/Loremaker/Maps/IslandHeightMapGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker.Maps
+{
+    public class IslandHeightMapGenerator : DefaultHeightMapGenerator
+    {
+        public int Margin { get; set; }
+
+        public IslandHeightMapGenerator()
+        {
+            this.Margin = 1;
+        }
+
+        protected override void SeedMap(double[,] map)
+        {
+            var width = map.GetLength(0);
+            var height = map.GetLength(1);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int m = 0; m < this.Margin; m++)
+                {
+                    map[x, 0 + m] = 0.01;
+                    map[x, height - 1 - m] = 0.01;
+                }
+            }
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int m = 0; m < this.Margin; m++)
+                {
+                    map[0 + m, y] = 0.01;
+                    map[width - 1 - m, y] = 0.01;
+                }
+            }
+        }
+    }
+}
