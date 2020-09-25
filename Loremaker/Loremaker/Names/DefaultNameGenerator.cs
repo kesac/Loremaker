516ab30550762f4e0b64fc1430345720e536645a
kesac <kevin@turtlesort.com>
2020-09-24 23:41:06 -0600

    Updated DefaultNameGenerator to use Syllabore 1.1

--- Loremaker/Loremaker/Names/DefaultNameGenerator.cs
+++ Loremaker/Loremaker/Names/DefaultNameGenerator.cs
@@ -11,74 +11,81 @@ namespace Loremaker.Names
     public class DefaultNameGenerator : INameGenerator
     {
 
-        private NameGenerator NameGenerator { get; set; }
+        private NameGenerator GeneralNames { get; set; }
         private Random Random { get; set; }
 
         public DefaultNameGenerator()
         {
-            var provider = new StandaloneSyllableProvider(); 
-            var validator = new StandaloneNameValidator();
-            this.NameGenerator = new NameGenerator(provider, validator);
 
             this.Random = new Random();
 
-            this.NameGenerator.MinimumSyllables = 2;
-            this.NameGenerator.MaximumSyllables = 2;
+            this.GeneralNames = new NameGenerator()
+                .UsingProvider(p => p
+                    .WithVowels("aeio")
+                    .WithLeadingConsonants("strlpn")
+                    .WithTrailingConsonantSequences("rt", "py"))
+                .UsingMutator(m => m
+                    .WithMutation(x => x.Syllables.Add("gard"))
+                    .WithMutation(x => x.Syllables.Insert(0, "gran")))
+                .UsingValidator(v => v
+                    .InvalidateRegex(@"(\w)\1\1"))
+                .LimitMutationChance(0.50)
+                .LimitSyllableCount(2,3);
         }
 
         public string Next()
         {
-            return this.NameGenerator.Next();
+            return this.GeneralNames.Next();
         }
 
         public string NextContinentName()
         {
-            return this.NameGenerator.Next(3);
+            return this.GeneralNames.Next(3);
         }
 
         public string NextFamilyName()
         {
-            return this.NameGenerator.Next(1 + this.Random.Next(3));
+            return this.GeneralNames.Next(1 + this.Random.Next(3));
         }
 
         public string NextGovernmentName()
         {
-            return this.NameGenerator.Next();
+            return this.GeneralNames.Next();
         }
 
         public string NextGroupName()
         {
-            return this.NameGenerator.Next();
+            return this.GeneralNames.Next();
         }
 
         public string NextGuildName()
         {
-            return this.NameGenerator.Next() + " Guild";
+            return this.GeneralNames.Next() + " Guild";
         }
 
         public string NextName()
         {
-            return this.NameGenerator.Next();
+            return this.GeneralNames.Next();
         }
 
         public string NextRegionName()
         {
-            return this.NameGenerator.Next();
+            return this.GeneralNames.Next();
         }
 
         public string NextReligiousOrderName()
         {
-            return "Order of " + this.NameGenerator.Next();
+            return "Order of " + this.GeneralNames.Next();
         }
 
         public string NextSettlementName()
         {
-            return this.NameGenerator.Next(2 + this.Random.Next(2));
+            return this.GeneralNames.Next(2 + this.Random.Next(2));
         }
 
         public string NextWorldName()
         {
-            return this.NameGenerator.Next();
+            return this.GeneralNames.Next();
         }
     }
 }
