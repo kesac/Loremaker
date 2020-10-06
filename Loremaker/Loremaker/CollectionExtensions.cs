335275de1b6eae421a1912e3d89d6c25607015c9
kesac <kevin@turtlesort.com>
2020-10-06 01:16:34 -0600

    Added extension method to combine Dictionaries. ListExtensions is now CollectionExtensions.

--- Loremaker/Loremaker/CollectionExtensions.cs
+++ Loremaker/Loremaker/CollectionExtensions.cs
@@ -4,13 +4,21 @@ using System.Text;
 
 namespace Loremaker
 {
-    public static class ListExtensions
+    public static class CollectionExtensions
     {
         public static readonly Random Random = new Random();
 
         public static string GetRandom(this List<string> list)
         {
             return list[Random.Next(list.Count)];
+        }
+
+        public static void AddRange(this Dictionary<string, string> to, Dictionary<string, string> from)
+        {
+            foreach(var fromKey in from.Keys)
+            {
+                to[fromKey] = from[fromKey];
+            }
         }
 
     }
