7decef8dc19a7df3ef05a81129cb3b363a62dbff
kesac <kevin@turtlesort.com>
2020-10-06 00:32:07 -0600

    Turned text generation methods originally specific to TextTemplate into extension methods for all Text* classes to use

---
+++ Loremaker/Loremaker/Text/TextExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Loremaker.Text
+{
+    public static class TextExtensions
+    {
+        public static bool HasContextClues(this string s)
+        {
+            return Regex.IsMatch(s, @"(\[[^\]]+\])");
+        }
+        public static string RemoveSquareBrackets(this string s)
+        {
+            return s.Replace("[", string.Empty).Replace("]", string.Empty);
+        }
+
+        public static List<string> GetContextClues(this string s)
+        {
+            var result = new List<string>();
+            if (s.HasContextClues())
+            {
+                foreach (var m in Regex.Matches(s, @"(\[[^\]]+\])"))
+                {
+                    var capture = m.ToString();
+                    var value = capture.RemoveSquareBrackets();
+                    result.Add(value.ToLower());
+                }
+            }
+            return result;
+        }
+    }
+}
