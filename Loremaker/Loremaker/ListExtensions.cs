ba07d6f65facc1df8f5d41410c6f7091485e2d4a
kesac <kevin@turtlesort.com>
2020-10-06 00:32:29 -0600

    Added ListExtensions class with GetRandom() extension method

---
+++ Loremaker/Loremaker/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker
+{
+    public static class ListExtensions
+    {
+        public static readonly Random Random = new Random();
+
+        public static string GetRandom(this List<string> list)
+        {
+            return list[Random.Next(list.Count)];
+        }
+
+    }
+}
