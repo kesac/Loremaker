b4b988f1a21fcad2d56dee9685b297a580ee51f0
kesac <kevin@turtlesort.com>
2020-10-06 00:31:26 -0600

    Added TextChain for creating a larger body of text by sequencing text templates and using context  tags

---
+++ Loremaker/Loremaker/Text/TextChain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loremaker.Text
+{
+    public class TextChain
+    {
+
+        private List<TextTemplate> Templates;
+        private Dictionary<string, TextEntity> GlobalEntities;
+
+        public TextChain()
+        {
+            this.Templates = new List<TextTemplate>();
+            this.GlobalEntities = new Dictionary<string, TextEntity>();
+        }
+
+        public TextChain DefineGlobally(string key, Func<TextEntity, TextEntity> configureEntity)
+        {
+            var e = configureEntity(new TextEntity(key));
+
+            if (this.GlobalEntities.Keys.Contains(key))
+            {
+                throw new InvalidOperationException(string.Format("A global entity with key '{0}' was previously defined.", key));
+            }
+            else
+            {
+                this.GlobalEntities[key] = e;
+            }
+
+            return this;
+        }
+
+        public TextChain Append(string template, Func<TextTemplate, TextTemplate> configure)
+        {
+            this.Templates.Add(configure(new TextTemplate(template)));
+            return this;
+        }
+
+        public string Next()
+        {
+            var result = new StringBuilder();
+            var currentContext = new List<string>();
+
+            foreach(var template in this.Templates)
+            {
+                if (template.IsFulfilledBy(currentContext.ToArray()))
+                {
+                    var output = template.NextOutput(this.GlobalEntities);
+                    currentContext.AddRange(output.Context); // adds new context hints
+
+                    result.Append(output.Value);
+                    result.Append(" ");
+                }
+            }
+
+            return result.ToString().Trim();
+        }
+
+    }
+}
