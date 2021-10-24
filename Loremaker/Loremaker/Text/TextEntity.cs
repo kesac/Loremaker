b579f33889a1d601f48165e7a8029088a83b2cf4
kesac <kesac@users.noreply.github.com>
2021-10-24 14:40:44 -0600

    Split TextEntity into two classes + TextEntity now only supports one primary value + Introduced TextEntityPool which represents one or more TextEntities instead of just string values + Previously, determines, adjectives, name generation, etc. always applied to all values in a TextEntity + This change now permits TextEntity values to have individualized determiners, adjectives, name generation, etc. in a TextTemplate

--- Loremaker/Loremaker/Text/TextEntity.cs
+++ Loremaker/Loremaker/Text/TextEntity.cs
@@ -1,39 +1,31 @@
-﻿using Syllabore;
+﻿using Loremaker.Names;
+using Syllabore;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Loremaker.Text
 {
-    /// <summary>
-    /// Represents one or more entities that can be captured
-    /// as a single word of text. Entities can have adjectives,
-    /// determiners, or form part of a name. TextEntities produce
-    /// <see cref="TextOutput"/>.
-    /// </summary>
     public class TextEntity : ITextGenerator
     {
         private Random Random { get; set; }
-
-        public string Id { get; set; }
-        public List<string> Objects { get; set; }
+        public string Value { get; set; }
         public List<string> Adjectives { get; set; }
         public List<string> Determiners { get; set; }
         public INameGenerator NameGenerator { get; set; }
 
-        public TextEntity(string key)
+
+        public TextEntity(string value = "")
         {
-            this.Id = key;
+            this.Value = value;
             this.Random = new Random();
-            this.Objects = new List<string>();
             this.Adjectives = new List<string>();
-            this.Determiners = new List<string>();   
+            this.Determiners = new List<string>();
         }
 
-        public TextEntity As(params string[] objects)
+        public TextEntity UsingValue(string value)
         {
-            this.Objects.AddRange(objects);
+            this.Value = value;
             return this;
         }
 
@@ -49,7 +41,7 @@ namespace Loremaker.Text
             return this;
         }
 
-        public TextEntity UsingNames(INameGenerator nameGenerator)
+        public TextEntity UsingNameGenerator(INameGenerator nameGenerator)
         {
             this.NameGenerator = nameGenerator;
             return this;
@@ -63,82 +55,33 @@ namespace Loremaker.Text
 
         public string Next()
         {
-            return NextOutput().Value;
+            return this.NextOutput().Value;
         }
 
         public TextOutput NextOutput()
         {
-            var result = new StringBuilder();
-            var output = new TextOutput();
-            var context = new List<string>();
+            var result = new TextOutput();
+            var builder = new StringBuilder();
 
-            if (this.NameGenerator != null)
+            if(this.Determiners.Count > 0)
             {
-                result.Append(" ");
-                result.Append(this.NameGenerator.Next());
-            }
-
-            if (this.Objects.Count > 0)
-            {
-
-                var o = this.Objects.GetRandom();
-
-                if (o.HasContextClues())
-                {
-                    context.AddRange(o.GetContextClues());
-                    o = o.RemoveSquareBrackets();
-                }
-
-                result.Append(" ");
-                result.Append(o);
+                builder.Append(this.Determiners[this.Random.Next(this.Determiners.Count)] + " ");
             }
 
             if (this.Adjectives.Count > 0)
             {
-
-                var adjective = this.Adjectives.GetRandom();
-
-                if (adjective.HasContextClues())
-                {
-                    context.AddRange(adjective.GetContextClues());
-                    adjective = adjective.RemoveSquareBrackets();
-                }
-
-                result.Insert(0, adjective);
-                result.Insert(0, " ");
+                builder.Append(this.Adjectives[this.Random.Next(this.Adjectives.Count)] + " ");
             }
 
-            if (this.Determiners.Count > 0)
+            if (this.NameGenerator != null)
             {
-
-                var determiner = this.Determiners.GetRandom();
-
-                if (determiner.HasContextClues())
-                {
-                    context.AddRange(determiner.GetContextClues());
-                    determiner = determiner.RemoveSquareBrackets();
-                }
-
-                if (determiner == "a" && result.ToString().Trim().StartsWithVowel())
-                {
-                    determiner = "an";
-                }
-                else if (determiner == "an" && !result.ToString().Trim().StartsWithVowel())
-                {
-                    determiner = "a";
-                }
-
-                result.Insert(0, determiner);
-                result.Insert(0, " ");
+                builder.Append(this.NameGenerator.Next() + " ");
             }
 
-            output.Value = result.ToString().Trim();
-            output.Context.AddRange(context);
-            return output;
-            
+            builder.Append(this.Value);
+
+            return new TextOutput(builder.ToString().Trim());
+
         }
-
-
-
     }
 }
---
+++ Loremaker/Loremaker/Text/TextEntityPool.cs
@@ -0,0 +1,171 @@
+﻿using Syllabore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Loremaker.Text
+{
+    /// <summary>
+    /// Represents one or more entities that can be captured
+    /// as a single word of text. Entities can have adjectives,
+    /// determiners, or form part of a name. TextEntities produce
+    /// <see cref="TextOutput"/>.
+    /// </summary>
+    public class TextEntityPool : ITextGenerator
+    {
+        private Random Random { get; set; }
+        public List<TextEntity> Entities { get; set; }
+
+        private List<TextEntity> LastAddedEntities { get; set; }
+
+        public TextEntityPool()
+        {
+            this.Random = new Random();
+            this.Entities = new List<TextEntity>();
+        }
+
+        public TextEntityPool As(params string[] objects)
+        {
+            this.LastAddedEntities = objects.Select(x => new TextEntity(x)).ToList();
+            this.Entities.AddRange(this.LastAddedEntities);
+            return this;
+        }
+
+        public TextEntityPool As(Func<TextEntity, TextEntity> configure)
+        {
+            var entity = configure(new TextEntity(string.Empty));
+            this.LastAddedEntities = new List<TextEntity>() { entity };
+            this.Entities.Add(entity);
+            return this;
+        }
+
+        public TextEntityPool ApplyAdjectives(params string[] adjectives)
+        {
+            this.LastAddedEntities.ForEach(x => x.Adjectives.AddRange(adjectives));
+            return this;
+        }
+
+        public TextEntityPool ApplyAdjectivesToAll(params string[] adjectives)
+        {
+            this.Entities.ForEach(x => x.Adjectives.AddRange(adjectives));
+            return this;
+        }
+
+        public TextEntityPool ApplyDeterminers(params string[] determiners)
+        {
+            this.LastAddedEntities.ForEach(x => x.Determiners.AddRange(determiners));
+            return this;
+        }
+
+        public TextEntityPool ApplyDeterminersToAll(params string[] determiners)
+        {
+            this.Entities.ForEach(x => x.Determiners.AddRange(determiners));
+            return this;
+        }
+
+        public TextEntityPool ApplyNameGenerator(INameGenerator nameGenerator)
+        {
+            this.LastAddedEntities.ForEach(x => x.NameGenerator = nameGenerator);
+            return this;
+        }
+
+        public TextEntityPool ApplyNameGeneratorToAll(INameGenerator nameGenerator)
+        {
+            this.Entities.ForEach(x => x.NameGenerator = nameGenerator);
+            return this;
+        }
+
+        public TextEntityPool ApplyNameGenerator(Func<NameGenerator, NameGenerator> configure)
+        {
+            var generator = configure(new NameGenerator());
+            this.LastAddedEntities.ForEach(x => x.NameGenerator = generator);
+            return this;
+        }
+
+        public TextEntityPool ApplyNameGeneratorToAll(Func<NameGenerator, NameGenerator> configure)
+        {
+            var generator = configure(new NameGenerator());
+            this.Entities.ForEach(x => x.NameGenerator = generator);
+            return this;
+        }
+
+        public string Next()
+        {
+            return NextOutput().Value;
+        }
+
+        public TextOutput NextOutput()
+        {
+            var builder = new StringBuilder();
+            var result = new TextOutput();
+            var context = new List<string>();
+
+
+            if (this.Entities.Count > 0)
+            {
+
+                var o = this.Entities.GetRandom<TextEntity>().Next();
+
+                if (o.HasContextClues())
+                {
+                    context.AddRange(o.GetContextClues());
+                    o = o.RemoveSquareBrackets();
+                }
+
+                builder.Append(" ");
+                builder.Append(o);
+            }
+
+            /*
+            if (this.Adjectives.Count > 0)
+            {
+
+                var adjective = this.Adjectives.GetRandom();
+
+                if (adjective.HasContextClues())
+                {
+                    context.AddRange(adjective.GetContextClues());
+                    adjective = adjective.RemoveSquareBrackets();
+                }
+
+                result.Insert(0, adjective);
+                result.Insert(0, " ");
+            }
+
+            if (this.Determiners.Count > 0)
+            {
+
+                var determiner = this.Determiners.GetRandom();
+
+                if (determiner.HasContextClues())
+                {
+                    context.AddRange(determiner.GetContextClues());
+                    determiner = determiner.RemoveSquareBrackets();
+                }
+
+                if (determiner == "a" && result.ToString().Trim().StartsWithVowel())
+                {
+                    determiner = "an";
+                }
+                else if (determiner == "an" && !result.ToString().Trim().StartsWithVowel())
+                {
+                    determiner = "a";
+                }
+
+                result.Insert(0, determiner);
+                result.Insert(0, " ");
+            }
+            /**/
+
+            result.Value = builder.ToString().Trim();
+            result.Context.AddRange(context);
+            return result;
+            
+        }
+
+
+
+    }
+}
