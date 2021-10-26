efd8e176c55e4abf117122f573236279e5ab0cf6
kesac <kesac@users.noreply.github.com>
2021-10-25 23:57:47 -0600

    Added convenience class for random number rolls

---
+++ Loremaker/Loremaker/Chance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker
+{
+    public static class Chance
+    {
+        private static Random Random = new Random();
+
+        public static bool Roll(double chance)
+        {
+            return Chance.Random.NextDouble() < chance;
+        }
+
+        public static int Between(int min, int max)
+        {
+            return Chance.Random.Next(max - min + 1) + min;
+        }
+
+    }
+}
