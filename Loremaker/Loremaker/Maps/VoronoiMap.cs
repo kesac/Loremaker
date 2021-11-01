551cf32ce329110e6feb737bae482539a19db50c
kesac <kesac@users.noreply.github.com>
2021-10-31 23:12:38 -0600

    Added a VoronoiMap generator + This integrates DelaunatorSharp into Loremaker via Archigen

---
+++ Loremaker/Loremaker/Maps/VoronoiMap.cs
@@ -0,0 +1,12 @@
+﻿using DelaunatorSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker.Maps
+{
+    public class VoronoiMap : Delaunator
+    {
+        public VoronoiMap(IPoint[] points) : base(points) { }
+    }
+}
---
+++ Loremaker/Loremaker/Maps/VoronoiMapGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Archigen;
+using DelaunatorSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker.Maps
+{
+    public class VoronoiMapGenerator : IGenerator<VoronoiMap>
+    {
+        public IGenerator<IPoint[]> PointsGenerator { get; set; }
+
+        public VoronoiMapGenerator(IGenerator<IPoint[]> generator)
+        {
+            this.PointsGenerator = generator;
+        }
+
+        public VoronoiMap Next()
+        {
+            var points = this.PointsGenerator.Next();
+            var d = new VoronoiMap(points);
+
+            return d;
+        }
+
+    }
+}
