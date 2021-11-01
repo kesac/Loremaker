c0bef8a4fd2db29972b14ea52e00b7603cb4928e
kesac <kesac@users.noreply.github.com>
2021-10-31 23:13:51 -0600

    Added new Map class + Represents a 2D map + Has a height map to describe terrain elevation + Has a voronoi map to describe sub-areas of the map

---
+++ Loremaker/Loremaker/Maps/Map.cs
@@ -0,0 +1,20 @@
+﻿using DelaunatorSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker.Maps
+{
+    public class Map
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public double[,] HeightMap { get; set; }
+        public VoronoiMap VoronoiMap { get; set; }
+
+        public Map()
+        {
+            
+        }
+    }
+}
