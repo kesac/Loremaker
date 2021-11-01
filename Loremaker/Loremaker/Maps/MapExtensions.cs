13cc1d7abafef1a197540cfb3f1f8b65a63bf7df
kesac <kesac@users.noreply.github.com>
2021-10-31 23:14:39 -0600

    Added new extension methods for maps

---
+++ Loremaker/Loremaker/Maps/MapExtensions.cs
@@ -0,0 +1,27 @@
+﻿using DelaunatorSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loremaker.Maps
+{
+    public static class MapExtensions
+    {
+
+        public static IPoint Average(this IPoint[] points)
+        {
+            return new DelaunatorSharp.Point(points.Average(x => x.X), points.Average(x => x.Y));
+        }
+
+        public static int AverageX(this IVoronoiCell cell)
+        {
+            return (int)cell.Points.Average(x => x.X);
+        }
+
+        public static int AverageY(this IVoronoiCell cell)
+        {
+            return (int)cell.Points.Average(x => x.Y);
+        }
+    }
+}
