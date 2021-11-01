793848b790638642f2ff81e42c6e05e2d7bcd272
kesac <kesac@users.noreply.github.com>
2021-10-31 23:14:08 -0600

    Added a new MapGenerator

---
+++ Loremaker/Loremaker/Maps/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Archigen;
+using DelaunatorSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker.Maps
+{
+    public class MapGenerator : Generator<Map>
+    {
+        public MapGenerator(int width, int height)
+        {
+            this.ForProperty<double[,]>(x => x.HeightMap, new IslandHeightMapGenerator(3).UsingVarianceDrop(0.4).UsingSize(width, height));
+            this.ForProperty<VoronoiMap>(x => x.VoronoiMap, new VoronoiMapGenerator(new PointsGenerator(width, height, 20)));
+            this.ForProperty<int>(x => x.Width, width);
+            this.ForProperty<int>(x => x.Height, height);
+        }
+    }
+}
