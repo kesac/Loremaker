2848415e3f22a09189342d7a7ec08af654b4c5a9
kesac <kesac@users.noreply.github.com>
2021-11-07 22:52:08 -0700

    Add Loremaker-specific point class + This means we don't need to depend on System.Drawing or other libraries for now

---
+++ Loremaker/Loremaker/Maps/MapPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker.Maps
+{
+    public struct MapPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public MapPoint(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
