1dcd0d4c95de15b35b8871b149684918102510a7
kesac <kesac@users.noreply.github.com>
2021-11-07 22:57:24 -0700

    Map renderer experimentation updates

--- Loremaker/Loremaker.Example.MapRenderer/Program.cs
+++ Loremaker/Loremaker.Example.MapRenderer/Program.cs
@@ -9,6 +9,7 @@ using System;
 using System.IO;
 using SixLabors.ImageSharp.Drawing.Processing.Processors.Drawing;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Loremaker.Example.MapRenderer
 {
@@ -45,7 +46,7 @@ namespace Loremaker.Example.MapRenderer
             // Generate(islandGenerator, "Island", 1);
 
 
-            var mapGenerator = new MapGenerator(1000, 1000);
+            var mapGenerator = new MapGenerator(1000, 1000, 0.20);
             var map = mapGenerator.Next();
             DrawCellmap(map);
         }
@@ -80,64 +81,74 @@ namespace Loremaker.Example.MapRenderer
             }
         }
 
+        private static Color GetColor(MapCell cell, double landThreshold)
+        {
+            var result = new Rgba32() { A = 255 };
+            var elevation = cell.Elevation;
+
+            if (cell.IsWater)
+            {
+                result.R = 0;
+                result.G = (byte)((150 - 30) * elevation / landThreshold + 30);
+                result.B = (byte)(elevation < (landThreshold / 2) ? ((255 - 150) * elevation / (landThreshold / 2) + 150) : 255);
+            }
+            else
+            {
+                result.R = 30;
+                result.G = (byte)((255 - 155) * elevation / (1 - landThreshold) + 155);
+                result.B = 0;
+            }
+
+            return result;
+        }
+
         private static void DrawCellmap(Map map)
         {
             var start = DateTime.Now;
             var output = "cellmap.png";
 
+            var e = map.Cells.Values.ElementAt(10);
+
+            var highlight = new List<int>() { e.CellId };
+            highlight.AddRange(e.AdjacentCellIds);
+
             using (var image = new Image<Rgba32>(map.Width, map.Height))
             {
 
-                var cells = map.VoronoiMap.GetVoronoiCells();
-
-                foreach (var cell in cells)
+                foreach (var cell in map.Cells.Values)
                 {
-                    if (cell.Points.Length > 2)
+                    if (cell.Shape.Count > 2)
                     {
-                        var elevation = map.HeightMap[cell.AverageX(), cell.AverageY()];
-                        var color = new Rgba32() { A = 255 };
-
-                        if(elevation < 0.20)
-                        {
-                            color.R = 0;
-                            color.G = (byte)((150-30) * elevation/0.20 + 30);
-                            color.B = (byte)(elevation < 0.10 ? ((255 - 150) * elevation / 0.10 + 150) : 255);
-                        }
-                        else
-                        {
-                            color.R = 30;
-                            color.G = (byte)((255 - 155) * elevation / 0.80 + 155);
-                            color.B = 0;
-                        }
+                        var color = GetColor(cell, map.LandThreshold);
 
                         image.Mutate(x => x
-                            .DrawLines(new Pen(color, 3f), cell.Points.ToPointFArray())
+                            .DrawLines(new Pen(color, 3f), cell.Shape.Select(p => new PointF(p.X,p.Y)).ToArray())
                         );
                     }
                 }
 
-                foreach (var cell in cells)
+                foreach (var cell in map.Cells.Values)
                 {
-                    if (cell.Points.Length > 2)
+                    if (cell.Shape.Count > 2)
                     {
-                        var elevation = map.HeightMap[cell.AverageX(), cell.AverageY()];
-                        var color = new Rgba32() { A = 255 };
-
-                        if (elevation < 0.20)
-                        {
-                            color.R = 0;
-                            color.G = (byte)((150 - 30) * elevation / 0.20 + 30);
-                            color.B = (byte)(elevation < 0.10 ? ((255 - 150) * elevation / 0.10 + 150) : 255);
-                        }
-                        else
-                        {
-                            color.R = 30;
-                            color.G = (byte)((255 - 155) * elevation / 0.80 + 155);
-                            color.B = 0;
-                        }
+                        var color = GetColor(cell, map.LandThreshold);
 
                         image.Mutate(x => x
-                            .FillPolygon(color, cell.Points.ToPointFArray())
+                            .FillPolygon(color, cell.Shape.Select(p => new PointF(p.X, p.Y)).ToArray())
+                        );
+                    }
+                }
+
+                foreach (var cell in map.Cells.Values)
+                {
+                    if(cell.Shape.Count > 2 && cell.IsCoast)
+                    {
+                        // var color = new Rgba32() { R = 150, B = 25, G = 150, A = 255 };
+                        var color = Color.Red;
+
+                        image.Mutate(x => x
+                            //.FillPolygon(color, cell.Shape.Select(p => new PointF(p.X, p.Y)).ToArray())
+                            .DrawLines(new Pen(color, 3f), new PointF(cell.Center.X, cell.Center.Y), new PointF(cell.Center.X, cell.Center.Y))
                         );
                     }
                 }
