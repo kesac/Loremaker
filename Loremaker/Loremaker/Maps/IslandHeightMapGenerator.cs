b45c0292fc34e776682ed1ca7c7d156f2f284610
kesac <kesac@users.noreply.github.com>
2021-11-11 00:26:56 -0700

    IslandHeightMapGenerator now gently fades land masses out as they approach the edge of the map

--- Loremaker/Loremaker/Maps/IslandHeightMapGenerator.cs
+++ Loremaker/Loremaker/Maps/IslandHeightMapGenerator.cs
@@ -4,46 +4,82 @@ using System.Text;
 
 namespace Loremaker.Maps
 {
-    /// <summary>
-    /// Identical to the DefaultHeightMapGenerator except it seeds the borders
-    /// of the map with a uniform low value. This causes generated height maps
-    /// to look like an island or continent.
-    /// </summary>
     public class IslandHeightMapGenerator : HeightMapGenerator
     {
-        /// <summary>
-        /// Essentially how thick the space between the map
-        /// border and land should be.
-        /// </summary>
+        public double RadiusBuffer { get; set; }
+        public double DropOffPercentage { get; set; }
+        public double DropOffPercentageVariance { get; set; }
         public int Margin { get; set; }
 
-        public IslandHeightMapGenerator(int margin)
+        public IslandHeightMapGenerator(int width, int height) : this()
         {
-            this.Margin = margin;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public IslandHeightMapGenerator()
+        {
+            this.RadiusBuffer = 300;
+            this.VarianceDropModifier = 0.55;
+            this.DropOffPercentage = 0.15;
+            this.DropOffPercentageVariance = 0.02;
+            this.Margin = 0;
+        }
+
+        private double GetDropOffPercentage()
+        {
+            return this.DropOffPercentage + ((this.DropOffPercentageVariance*2) * Random.NextDouble()) - this.DropOffPercentageVariance;
         }
 
         protected override void SeedMap(double[,] map)
         {
-            var width = map.GetLength(0);
-            var height = map.GetLength(1);
+            map[0, 0] = 0.01;
+            map[0, map.GetLength(1) - 1] = 0.01;
+            map[map.GetLength(0) - 1, 0] = 0.01;
+            map[map.GetLength(0) - 1, map.GetLength(1) - 1] = 0.01;
+        }
 
+        public override double[,] Next()
+        {
+            return this.Next(this.Width, this.Height);
+        }
+
+        public override double[,] Next(int width, int height)
+        {
+            var result = base.Next(width, height);
+
+            var outerRadius = (double)(width / 2) - this.Margin;
+            var innerRadius = outerRadius - this.RadiusBuffer;
+            
             for (int x = 0; x < width; x++)
             {
-                for (int m = 0; m < this.Margin; m++)
+                for (int y = 0; y < height; y++)
                 {
-                    map[x, 0 + m] = 0.01;
-                    map[x, height - 1 - m] = 0.01;
+                    var distance = DistanceFromCenter(x, y, width, height);
+                    // var angleFromCenter = Math.Atan2(y - height/2, x - width/2);
+                    // var variance = Math.Sin(angleFromCenter * Math.PI) * 100;
+                    var variance = 0;
+
+                    if (distance > outerRadius + variance)
+                    {
+                        result[x, y] = result[x, y] * GetDropOffPercentage();
+                    }
+                    else if (distance > innerRadius + variance)
+                    {
+                        var gradient = 1 - ((distance - innerRadius) / this.RadiusBuffer);
+                        result[x, y] = Math.Max(result[x, y] * gradient, result[x, y] * GetDropOffPercentage());
+                    }
+
                 }
             }
 
-            for (int y = 0; y < height; y++)
-            {
-                for (int m = 0; m < this.Margin; m++)
-                {
-                    map[0 + m, y] = 0.01;
-                    map[width - 1 - m, y] = 0.01;
-                }
-            }
+            return result;
         }
+
+        private double DistanceFromCenter(int x, int y, int width, int height)
+        {
+            return Math.Sqrt(Math.Pow(x - width/2, 2) + Math.Pow(y - height / 2, 2));
+        }
+
     }
 }
---
+++ Loremaker/Loremaker/Maps/OldIslandHeightMapGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker.Maps
+{
+    /// <summary>
+    /// Identical to the DefaultHeightMapGenerator except it seeds the borders
+    /// of the map with a uniform low value. This causes generated height maps
+    /// to look like an island or continent.
+    /// </summary>
+    public class OldIslandHeightMapGenerator : HeightMapGenerator
+    {
+
+        /// <summary>
+        /// Essentially how thick the space between the map
+        /// border and land should be.
+        /// </summary>
+        public int Margin { get; set; }
+
+        public OldIslandHeightMapGenerator(int margin)
+        {
+            this.Margin = margin;
+        }
+
+        private double GetRandomSeaElevation()
+        {
+            return 0.15 + (0.02 * Random.NextDouble());
+        }
+
+        protected override void SeedMap(double[,] map)
+        {
+            
+            var width = this.Width;
+            var height = this.Height;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int m = 0; m < this.Margin; m++)
+                {
+                    map[i, 0 + m] = GetRandomSeaElevation();
+                    map[i, height - 1 - m] = GetRandomSeaElevation();
+
+                    map[0 + m, i] = GetRandomSeaElevation();
+                    map[width - 1 - m, i] = GetRandomSeaElevation();
+                }
+            }
+
+            var count = (width + height) / 20;
+
+            for(int i = 0; i < count; i++)
+            {
+                var x = Random.Next(this.Margin, width - this.Margin);
+                var y = Random.Next(this.Margin, height - this.Margin);
+
+                for(int j = x; j < 10 && j < width; j++)
+                {
+                    for(int h = y; h < 10 && h < height; h++)
+                    {
+                        map[j, h] = GetRandomSeaElevation();
+                    }
+                }
+            }
+        }
+    }
+}
