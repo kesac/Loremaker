4981a41d4fdfba19af0f6c5e3c596aa381f7b912
kesac <kesac@users.noreply.github.com>
2021-11-13 00:10:17 -0700

    LandmassRegions are now called Territories

--- Loremaker/Loremaker/Territory.cs
+++ Loremaker/Loremaker/Territory.cs
@@ -7,18 +7,15 @@ using System.Text.Json.Serialization;
 
 namespace Loremaker
 {
-    public class LandmassRegion : Identifiable
+    public class Territory : Identifiable
     {
         public uint Id { get; set; }
         public string Name { get; set; }
-        public uint ParentId { get; set; }
-        [JsonIgnore]
-        public virtual Landmass Parent { get; set; }
         public List<uint> MapCellIds { get; set; }
         [JsonIgnore]
         public virtual List<MapCell> MapCells { get; set; }
 
-        public LandmassRegion()
+        public Territory()
         {
             this.MapCellIds = new List<uint>();
             this.MapCells = new List<MapCell>();
