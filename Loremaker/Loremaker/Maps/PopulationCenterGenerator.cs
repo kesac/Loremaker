3246f8e49a53dcc6edc83d5b937a2bfa993c4edb
kesac <kesac@users.noreply.github.com>
2021-11-13 00:09:20 -0700

    Enhanced population center generation + Number of pop centers increases depending on size of landmass + Pop centers can appear further inland now

--- Loremaker/Loremaker/Maps/PopulationCenterGenerator.cs
+++ Loremaker/Loremaker/Maps/PopulationCenterGenerator.cs
@@ -18,20 +18,31 @@ namespace Loremaker.Maps
             this.NameGenerator = nameGenerator;
         }
 
-        public List<PopulationCenter> Next()
+        private List<PopulationCenter> NextPopulation(Func<Landmass,bool> landmassCondition, Func<Landmass,int> maxSize, Func<MapCell,bool> mapCellCondition)
         {
             var result = new List<PopulationCenter>();
 
+            var offlimits = new List<uint>();
+            var maxTries = 100;
             uint count = 0;
 
-            foreach(var landmass in this.Landmasses)
+            foreach (var landmass in this.Landmasses.Where(x => landmassCondition(x)))
             {
-                var coasts = landmass.MapCells.Where(x => x.IsCoast).ToList();
-                var pops = Math.Max(1, landmass.MapCells.Count / 100);
-                
+                var coasts = landmass.MapCells.Where(x => mapCellCondition(x)).ToList();
+                var pops = maxSize(landmass);
+
                 for (int i = 0; i < pops; i++)
                 {
-                    var homecell = coasts.FindRandom<MapCell>();
+                    var homecell = coasts.GetRandom();
+                    var tries = 1;
+
+                    while (offlimits.Contains(homecell.Id) && tries < maxTries)
+                    {
+                        homecell = coasts.GetRandom();
+                        tries++;
+                    }
+
+                    if (tries >= maxTries) continue;
 
                     var pc = new PopulationCenter()
                     {
@@ -39,13 +50,39 @@ namespace Loremaker.Maps
                         MapCellId = homecell.Id,
                         Name = this.NameGenerator.Next(),
                         Type = PopulationCenterType.Town,
+                        Landmass = landmass,
+                        LandmassId = landmass.Id,
                         Id = count++
                     };
 
+                    offlimits.Add(homecell.Id);
+                    offlimits.AddRange(homecell.AdjacentMapCellIds);
+                    offlimits.AddRange(homecell.AdjacentMapCells.SelectMany(x => x.AdjacentMapCellIds));
                     result.Add(pc);
                 }
-                
+
             }
+
+            return result;
+        }
+
+        // TODO: Simplify
+        public List<PopulationCenter> Next()
+        {
+            var result = new List<PopulationCenter>();
+
+            var coastalPopulations = this.NextPopulation(
+                    landmass => landmass.Size > 2,
+                    landmass => (int)Math.Max(1, Math.Log(landmass.Size)/2),
+                    mapcell => mapcell.IsCoast);
+
+            var landlockedPopulations = this.NextPopulation(
+                    landmass => landmass.Size > 10,
+                    landmass => (int)Math.Max(0, landmass.Size/150),
+                    mapcell => mapcell.IsLand && !mapcell.IsCoast);
+
+            result.AddRange(coastalPopulations);
+            result.AddRange(landlockedPopulations);
 
             return result;
         }
