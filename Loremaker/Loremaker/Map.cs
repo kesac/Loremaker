2d2fa9a71f4a5fc1b6fbcbfdc7a2bf69e3ab4a33
kesac <kesac@users.noreply.github.com>
2021-11-14 15:53:37 -0700

    Map collection changes + Points and cells are now only stored in Dictionaries instead of Lists with Dictionaries as the latest version of System.Text.Json lets us serialize Dictionaries with non-string keys propertly + Moved tracking of Landmasses up to the World level

--- Loremaker/Loremaker/Map.cs
+++ Loremaker/Loremaker/Map.cs
@@ -10,24 +10,13 @@ namespace Loremaker
         public int Width { get; set; }
         public int Height { get; set; }
         public float LandThreshold { get; set; }
-        public List<MapPoint> MapPoints { get; set; }
-        [JsonIgnore]
-        public Dictionary<uint, MapPoint> MapPointsById { get; set; }
-        public List<MapCell> MapCells { get; set; }
-        [JsonIgnore]
-        public Dictionary<uint, MapCell> MapCellsById { get; set; }
-        public List<Landmass> Landmasses { get; set; }
-        [JsonIgnore]
-        public Dictionary<uint, Landmass> LandmassesById { get; set; }
+        public Dictionary<uint, MapPoint> MapPoints { get; set; }
+        public Dictionary<uint, MapCell> MapCells { get; set; }
 
         public Map()
         {
-            this.MapPoints = new List<MapPoint>();
-            this.MapPointsById = new Dictionary<uint, MapPoint>();
-            this.MapCells = new List<MapCell>();
-            this.MapCellsById = new Dictionary<uint, MapCell>();
-            this.Landmasses = new List<Landmass>();
-            this.LandmassesById = new Dictionary<uint, Landmass>();
+            this.MapPoints = new Dictionary<uint, MapPoint>();
+            this.MapCells = new Dictionary<uint, MapCell>();
         }
 
     }
