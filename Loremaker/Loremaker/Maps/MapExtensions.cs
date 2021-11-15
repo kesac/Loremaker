c8352022cf3bf38d1bc6373eaa05b47158829428
kesac <kesac@users.noreply.github.com>
2021-11-15 11:26:59 -0700

    Added new experimental territory generation algorithm

--- Loremaker/Loremaker/Maps/MapExtensions.cs
+++ Loremaker/Loremaker/Maps/MapExtensions.cs
@@ -19,6 +19,47 @@ namespace Loremaker.Maps
             return new MapPoint((int)points.Average(p => p.X), (int)points.Average(p => p.Y));
         }
 
+
+        public static double Distance(this MapPoint p1, MapPoint p2)
+        {
+            float x = p1.X - p2.X;
+            float y = p1.Y - p2.Y;
+
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+
+
+        /// <summary>
+        /// <para>
+        /// Given two groups A and B of map cells, this method returns the closest distance
+        /// between the center of any two cells P1 and P2, where P1 is a member of A and
+        /// P2 is a member of B.
+        /// </para>
+        /// <para>
+        /// This method is useful in determining the distance between two landmasses 
+        /// where their cells are the closest.
+        /// </para>
+        /// </summary>
+        public static double ShortestCellDistance(this List<MapCell> cells1, List<MapCell> cells2)
+        {
+            double shortestDistance = double.MaxValue;
+
+            foreach(var cell1 in cells1)
+            {
+                foreach(var cell2 in cells2)
+                {
+                    var distance = cell1.Center.Distance(cell2.Center);
+                    if (distance < shortestDistance)
+                    {
+                        shortestDistance = distance;
+                    }
+                }
+            }
+
+            return shortestDistance;
+        }
+
         public static DelaunatorSharp.IPoint Average(this DelaunatorSharp.IPoint[] points)
         {
             return new DelaunatorSharp.Point(points.Average(x => x.X), points.Average(x => x.Y));
--- Loremaker/Loremaker/Maps/TerritoryGenerator.cs
+++ Loremaker/Loremaker/Maps/TerritoryGenerator.cs
@@ -10,14 +10,175 @@ namespace Loremaker.Maps
     {
 
         private World World;
+        private IGenerator<string> TerritoryNameGenerator;
 
-        public TerritoryGenerator(World world)
+        public int MinimumTerritorySize { get; set; }
+        public int MaximumTerritorySize { get; set; }
+
+        public TerritoryGenerator(World world, IGenerator<string> nameGenerator)
         {
             this.World = world;
+            this.TerritoryNameGenerator = nameGenerator;
+            this.MinimumTerritorySize = 10;
+            this.MaximumTerritorySize = 500;
+        }
+
+        // New iteration of algorithm:
+        // Each landmass at or larger than MinimumLandmassSize is automatically its own territory
+        // Each landmass below MinimumLandmassSize is assigned to the closest territory
+        // For landmasses with a certain size, subdivide into smaller territories using
+        //   similar method of growth as last algorithm
+
+        public List<Territory> Next()
+        {
+            var result = new List<Territory>();
+
+            // Each landmass of MinimumLandmassSize or more is automatically its own territory, at
+            // least initially before any subdivision occurs
+            foreach (var landmass in this.World.Landmasses.Values.Where(x => x.Size >= this.MinimumTerritorySize))
+            {
+                var territory = new Territory();
+
+                territory.Name = this.TerritoryNameGenerator.Next();
+                territory.MapCellIds.AddRange(landmass.MapCellIds);
+                territory.MapCells.AddRange(landmass.MapCells);
+                territory.Center = new MapPoint(
+                    (int)landmass.MapCells.Average(x => x.Center.X),
+                    (int)landmass.MapCells.Average(x => x.Center.Y)
+                );
+
+                result.Add(territory);
+            }
+
+            // Convert really large territories into smaller ones
+            while (result.Where(x => x.MapCells.Count > this.MaximumTerritorySize).FirstOrDefault() != null)
+            {
+                for (int i = result.Count - 1; i >= 0; i--)
+                {
+                    var territory = result[i];
+                    if (territory.MapCells.Count > this.MaximumTerritorySize)
+                    {
+                        var subdivisions = this.Subdivide(territory);
+                        result.Remove(territory);
+                        result.AddRange(subdivisions);
+                    }
+                }
+            }
+
+            // Each landmass of size under 3 is assigned to the closest territory
+            // Note that if there aren't any landmasses of size 3 or greater this will
+            // result in territory-less islands
+            foreach (var landmass in this.World.Landmasses.Values.Where(x => x.Size < this.MinimumTerritorySize))
+            {
+                var closestTerritory = result.FirstOrDefault();
+
+                if (closestTerritory != null)
+                {
+                    // var closestDistance = landmass.Center.Distance(closestTerritory.Center);
+                    var closestDistance = landmass.MapCells.ShortestCellDistance(closestTerritory.MapCells);
+
+                    foreach (var territory in result)
+                    {
+                        var distance = landmass.MapCells.ShortestCellDistance(territory.MapCells);
+                        if (distance < closestDistance)
+                        {
+                            closestDistance = distance;
+                            closestTerritory = territory;
+                        }
+                    }
+
+                    closestTerritory.MapCellIds.AddRange(landmass.MapCellIds);
+                    closestTerritory.MapCells.AddRange(landmass.MapCells);
+                }
+            }
+
+
+
+            uint id = 0;
+            foreach(var territory in result)
+            {
+                territory.Id = id++;
+            }
+
+            return result;
+        }
+
+        private Territory[] Subdivide(Territory original, int divisions = 2)
+        {
+
+            if(original.MapCells.Count < divisions)
+            {
+                throw new InvalidOperationException("The number of divisions cannot exceed the number of cells in the territory.");
+            }
+
+            var unclaimed = new List<uint>(); // For RemoveRandom()
+            unclaimed.AddRange(original.MapCellIds);
+
+            var territories = Enumerable.Range(0, divisions).Select(_ => new Territory()).ToArray();
+            var adjacencies = Enumerable.Range(0, divisions).Select(_ => new List<uint>()).ToArray();
+
+            for (int i = 0; i < divisions; i++)
+            {
+                var rootId = unclaimed.RemoveRandom();
+                var cell = this.World.Map.MapCells[rootId];
+                territories[i].MapCells.Add(cell);
+                territories[i].MapCellIds.Add(cell.Id);
+                adjacencies[i].AddRange(cell.AdjacentMapCellIds.Where(id => unclaimed.Contains(id)));
+            }
+
+            while(unclaimed.Count > 0)
+            {
+                bool expanded = false;
+
+                for (int i = 0; i < divisions && unclaimed.Count > 0; i++)
+                {
+                    if(Chance.Roll(0.5))
+                    {
+                        continue;
+                    }
+
+                    if(adjacencies[i].Count > 0)
+                    {
+                        var newAdjacencies = new List<uint>();
+
+                        foreach(var adjacentId in adjacencies[i])
+                        {
+                            if (unclaimed.Contains(adjacentId))
+                            {
+                                unclaimed.Remove(adjacentId);
+                                var adjacentCell = this.World.Map.MapCells[adjacentId];
+
+                                territories[i].MapCells.Add(adjacentCell);
+                                territories[i].MapCellIds.Add(adjacentId);
+                                expanded = true;
+
+                                foreach (var id in adjacentCell.AdjacentMapCellIds)
+                                {
+                                    if (unclaimed.Contains(id))
+                                    {
+                                        newAdjacencies.Add(id);
+                                    }
+                                }
+                            }
+                        }
+
+                        adjacencies[i] = newAdjacencies;   
+                    }
+                }
+
+                if(!expanded)
+                {
+                    // break;
+                }
+
+            }
+
+
+            return territories;
         }
 
         // TODO: Simplify
-        public List<Territory> Next()
+        public List<Territory> OldNext()
         {
             
             var unclaimedLand = new List<MapCell>();
