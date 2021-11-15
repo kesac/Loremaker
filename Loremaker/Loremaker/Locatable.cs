1061ef5e7fcccad44fd496079d204c4dff9ca5ee
kesac <kesac@users.noreply.github.com>
2021-11-15 13:08:05 -0700

    Added interface for entities with a center

---
+++ Loremaker/Loremaker/Locatable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker
+{
+    /// <summary>
+    /// Represents any entity with a locatable center
+    /// in 2D space.
+    /// </summary>
+    public interface Locatable
+    {
+        /// <summary>
+        /// The X coordinate representing the center of
+        /// a two dimensional entity.
+        /// </summary>
+        int X { get; set; }
+
+        /// <summary>
+        /// The Y coordinate representing the center of
+        /// a two dimensional entity.
+        /// </summary>
+        int Y { get; set; }
+    }
+}
