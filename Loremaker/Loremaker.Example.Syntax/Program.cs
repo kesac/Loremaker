c9f156b37a30dab957852ee65510a0f9da39a677
kesac <kevin@turtlesort.com>
2024-11-09 12:07:31 -0700

    Added example project for reading a .lore file

---
+++ Loremaker/Loremaker.Example.Syntax/Program.cs
@@ -0,0 +1,22 @@
+﻿using Loremaker.Text;
+using Syllabore;
+
+namespace Loremaker.Example.Syntax
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var template = TextTemplate.LoadFromFile("SyntaxTemplate.lore");
+
+            var names = new NameGenerator();
+            template.Substitutions.Add("placeName", names);
+            template.Substitutions.Add("person", names);
+
+            var result = template.Next();
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
