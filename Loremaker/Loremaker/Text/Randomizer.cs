66cc92dc21db4788b99a17757b23477231691829
kesac <kevin@turtlesort.com>
2024-11-09 13:58:45 -0700

    Added new string randomizer classes

---
+++ Loremaker/Loremaker/Text/Randomizer.cs
@@ -0,0 +1,40 @@
+﻿using Archigen;
+using System;
+using System.Collections.Generic;
+
+namespace Loremaker.Text
+{
+    /// <summary>
+    /// A generator that returns random elements
+    /// from a specified list.
+    /// </summary>
+    public class Randomizer<T> : IGenerator<T>
+    {
+        protected Random _random;
+
+        /// <summary>
+        /// The list of values that the generator will
+        /// use to generate output during calls to <see cref="Next()"/>.
+        /// All values are equally likely to be selected.
+        /// </summary>
+        public List<T> Values { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="Randomizer{T}"/>
+        /// that will return random values from the specified list.
+        /// </summary>
+        public Randomizer(List<T> values)
+        {
+            _random = new Random();
+            Values = values;
+        }
+
+        /// <summary>
+        /// Returns a random value from a list of strings.
+        /// </summary>
+        public virtual T Next()
+        {
+            return Values[_random.Next(Values.Count)];
+        }
+    }
+}
---
+++ Loremaker/Loremaker/Text/SubjectRandomizer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace Loremaker.Text
+{
+    /// <summary>
+    /// A generator that returns random 
+    /// subjects with leading adjectives and determiners.
+    /// </summary>
+    public class SubjectRandomizer : Randomizer<string>
+    {
+        /// <summary>
+        /// The list of determiners that the generator will
+        /// include in the output during calls to <see cref="Next()"/>.
+        /// This can be left empty. Determiners will appear before
+        /// adjectives and the subject. All values are equally likely
+        /// to be selected.
+        /// </summary>
+        public Randomizer<string> Determiners { get; set; }
+
+        /// <summary>
+        /// The list of adjectives that the generator will
+        /// include in the output during calls to <see cref="Next()"/>.
+        /// The adjectives will appear before the subject, but after
+        /// determiners. All values are equally likely to be selected.
+        /// </summary>
+        public Randomizer<string> Adjectives { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="SubjectRandomizer"/> that
+        /// will return subjects from the specified list.
+        /// </summary>
+        public SubjectRandomizer(List<string> values) : base(values) { }
+
+        /// <summary>
+        /// Sets the determiners that the generator will use.
+        /// </summary>
+        public void SetDeterminers(List<string> determiners)
+        {
+            Determiners = new Randomizer<string>(determiners);
+        }
+
+        /// <summary>
+        /// Sets the adjectives that the generator will use.
+        /// </summary>
+        public void SetAdjectives(List<string> adjectives)
+        {
+            Adjectives = new Randomizer<string>(adjectives);
+        }
+
+        /// <summary>
+        /// Generates a random subject in the format "[determiner] [adjective] [subject]".
+        /// </summary>
+        public override string Next()
+        {
+            var result = base.Next();
+
+            if (Adjectives != null && Adjectives.Values.Count > 0)
+            {
+                result = Adjectives.Next() + " " + result;
+            }
+
+            if (Determiners != null && Determiners.Values.Count > 0)
+            {
+                result = Determiners.Next() + " " + result;
+            }
+
+            return result;
+        }
+    }
+}
