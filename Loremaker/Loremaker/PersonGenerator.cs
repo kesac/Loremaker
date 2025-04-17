90389a3439ab7c1cff824342a02b5ae77d17b40e
kesac <kevin@turtlesort.com>
2025-04-16 23:39:06 -0600

    Added rudimentary person generator

---
+++ Loremaker/Loremaker/PersonGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Archigen;
+using Loremaker.Names;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loremaker
+{
+    public class PersonGenerator : IGenerator<Person>
+    {
+        private IGenerator<string> _nameGenerator;
+
+        public PersonGenerator()
+        {
+            _nameGenerator = new DefaultNameGenerator();
+        }
+
+        public Person Next()
+        {
+            var result = new Person();
+            result.GivenName = _nameGenerator.Next();
+            result.FamilyName = _nameGenerator.Next();
+            return result;
+        }
+
+    }
+}
