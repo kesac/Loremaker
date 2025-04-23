c66ebd5f0a96f4df50512066cc5d2de388a8c8df
kesac <kevin@turtlesort.com>
2025-04-22 20:38:08 -0600

    Added a new entity class for Items

---
+++ Loremaker/Loremaker/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Loremaker
+{
+    /// <summary>
+    /// Represents a fictional item in the world that can be used by fictional people.
+    /// </summary>
+    public class Item : Entity
+    {
+        [JsonIgnore]
+        public string Name
+        {
+            get => this.HasProperty("name") ? this["name"] : null;
+            set => this["name"] = value;
+        }
+
+        [JsonIgnore]
+        public string Description
+        {
+            get => this.HasProperty("description") ? this["description"] : null;
+            set => this["description"] = value;
+        }
+
+        public Item()
+        {
+            this.Type = "item";
+            this.Name = string.Empty;
+            this.Description = string.Empty;
+        }
+    }
+}
