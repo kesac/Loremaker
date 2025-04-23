9b4fdfa6071fa85c7df94dcc5b372206983f0cf9
kesac <kevin@turtlesort.com>
2025-04-22 20:48:37 -0600

    Updated name generation in the syntax example project

--- Loremaker/Loremaker.Example.Syntax/Program.cs
+++ Loremaker/Loremaker.Example.Syntax/Program.cs
@@ -1,22 +1,31 @@
-﻿using Loremaker.Text;
+﻿using Archigen;
+using Loremaker.Text;
 using Syllabore;
+using Syllabore.Fluent;
 
 namespace Loremaker.Example.Syntax
 {
+    /// <summary>
+    /// Loads the file "SyntaxTemplate.lore"
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
         {
             var template = TextTemplate.LoadFromFile("SyntaxTemplate.lore");
+            var names = new NameGenerator()
+                .Any(x => x
+                    .First("str")
+                    .Middle("ae"));
 
-            var names = new NameGenerator();
+            var personName = names.Next();
+
             template.Substitutions.Add("placeName", names);
-            template.Substitutions.Add("person", names);
+            template.Substitutions.Add("person", new ConstantValue<string>(personName));
 
             var result = template.Next();
 
             Console.WriteLine(result);
-
         }
     }
 }
